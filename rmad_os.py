080dbc410e70d238697f781e45f4d39ff5cb2f9f
lazarus <williamcswll.thepequod@gmail.com>
2025-04-02 07:28:44 -0700

    fixed issues hopefully

--- rmad_os.py
+++ rmad_os.py
@@ -16,25 +16,6 @@ import board
 from adafruit_rgb_display.rgb import color565
 from adafruit_rgb_display import st7789
 
-
-
-cs_pin = digitalio.DigitalInOut(board.CE0)
-dc_pin = digitalio.DigitalInOut(board.D25)
-reset_pin = None
-BAUDRATE = 64000000  # The pi can be very fast!
-# Create the ST7789 display:
-display = st7789.ST7789(
-    board.SPI(),
-    cs=cs_pin,
-    dc=dc_pin,
-    rst=reset_pin,
-    baudrate=BAUDRATE,
-    width=135,
-    height=240,
-    x_offset=53,
-    y_offset=40,
-)
-
 backlight = digitalio.DigitalInOut(board.D22)
 backlight.switch_to_output()
 backlight.value = True
